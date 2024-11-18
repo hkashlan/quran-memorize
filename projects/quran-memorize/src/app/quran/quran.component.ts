9e2c9168ac9a3da332d3b9fe9af2fa1ad201c764
hadi <hadi@hadis-MacBook-Pro.local>
2024-11-18 06:03:35 +0100

    before moving to cloud api

--- projects/quran-memorize/src/app/quran/quran.component.ts
+++ projects/quran-memorize/src/app/quran/quran.component.ts
@@ -1,15 +1,32 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, linkedSignal } from '@angular/core';
 import { Note, quranPages, Recitation } from './quran';
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { AudioQuranRecorder } from './audio';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+// import quran from '../../../quran.json';
 
 const pageNumber = (page: number) => ('00' + page).slice(-3);
 
 @Component({
   selector: 'app-quran',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './quran.component.html',
   styleUrl: './quran.component.scss',
 })
@@ -18,7 +35,7 @@ export class QuranComponent {
   pageNumber = input.required<number>();
   pageNumberString = computed(() => pageNumber(this.pageNumber()));
   nextPage = computed(() => pageNumber(+this.pageNumber() + 1));
-  prevPage = computed(() => pageNumber(+this.pageNumber() -1));
+  prevPage = computed(() => pageNumber(+this.pageNumber() - 1));
 
   page = computed(() => quranPages[this.pageNumber() - 1]);
   goToPageNumber: number = 0;
@@ -69,8 +86,7 @@ export class QuranComponent {
   }
 
   goToPage(page: number) {
-    if(page) {
-
+    if (page) {
       this.router.navigate(['/page/' + pageNumber(page)]);
     }
   }
